fc684efa781390f606eb3782523e721faa20a8ca
Lê Quang Duẩn <20110619@student.hcmute.edu.vn>
2024-10-13 21:47:02 +0700

    get product

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import AddDataController from "../controllers/admin/data/AddDataController.js";
 import AuthController from "../controllers/auth/AuthController.js";
 import UserController from "../controllers/user/UserController.js";
-
+import GuestController from "../controllers/guest/GuestController.js";
 export function initApplication(app) {
     BigInt.prototype.toJSON = function () {
         return this.toString();
@@ -14,4 +14,5 @@ export function initRoutes(app) {
     AddDataController.initRoutes(app);
     AuthController.initRoutes(app)
     UserController.initRoutes(app)
+    GuestController.initRoutes(app)
 }
